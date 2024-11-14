38919dfe08f25d367603ecdf5d336a4e06931d17
wendellnd <wendelldourado.wn@gmail.com>
2024-11-13 21:58:31 -0300

    feat: landing page && start hire page

---
+++ home/scripts.js
@@ -0,0 +1,23 @@
+function navigate(url) {
+    window.location.href = url;
+}
+
+function hire() {
+    navigate('../hireForm/index.html')
+}
+
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
