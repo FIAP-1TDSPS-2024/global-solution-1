0585e20e512e64637d643490b7549394953510ca
wendellnd <wendelldourado.wn@gmail.com>
2024-11-21 20:05:18 -0300

    feat: add validateForm function

--- hireForm/scripts.js
+++ hireForm/scripts.js
@@ -2,24 +2,68 @@ function navigate(url) {
   window.location.href = url;
 }
 
+function validateForm() {
+  const form = document.querySelector(".hireForm");
+  const cpf = form.querySelector('input[placeholder="CPF/CNPJ"]');
+  const name = form.querySelector('input[placeholder="Nome"]');
+  const email = form.querySelector('input[placeholder="Email"]');
+  const phone = form.querySelector('input[placeholder="Telefone"]');
+  const message = form.querySelector("textarea");
+  const buttonSubmit = document.getElementById("submit");
+
+  if (
+    !cpf.value ||
+    !name.value ||
+    !email.value ||
+    !phone.value ||
+    !message.value
+  ) {
+    buttonSubmit.disabled = true;
+    return;
+  }
+
+  cpf.classList.remove("invalidField");
+  name.classList.remove("invalidField");
+  email.classList.remove("invalidField");
+  phone.classList.remove("invalidField");
+  message.classList.remove("invalidField");
+
+  buttonSubmit.removeAttribute("disabled");
+}
+
 function hire() {
   const form = document.querySelector(".hireForm");
-  const cpf = form.querySelector('input[placeholder="CPF/CNPJ"]').value;
-  const name = form.querySelector('input[placeholder="Nome"]').value;
-  const email = form.querySelector('input[placeholder="Email"]').value;
-  const phone = form.querySelector('input[placeholder="Telefone"]').value;
-  const message = form.querySelector("textarea").value;
+  const cpf = form.querySelector('input[placeholder="CPF/CNPJ"]');
+  const name = form.querySelector('input[placeholder="Nome"]');
+  const email = form.querySelector('input[placeholder="Email"]');
+  const phone = form.querySelector('input[placeholder="Telefone"]');
+  const message = form.querySelector("textarea");
 
-  if (!cpf || !name || !email || !phone || !message) {
+  if (
+    !cpf.value ||
+    !name.value ||
+    !email.value ||
+    !phone.value ||
+    !message.value
+  ) {
+    cpf.classList.add("invalidField");
+    name.classList.add("invalidField");
+    email.classList.add("invalidField");
+    phone.classList.add("invalidField");
+    message.classList.add("invalidField");
+
     alert("Preencha todos os campos");
     return;
   }
 
-  if (cpf.length < 11) {
+  if (cpf.value.length < 11) {
+    cpf.classList.add("invalidField");
     alert("CPF/CNPJ inválido");
     return;
   }
-  if (phone.length < 10) {
+  if (phone.value.length < 10) {
+    phone.classList.add("invalidField");
+
     alert("Telefone inválido");
     return;
   }
@@ -30,3 +74,5 @@ function hire() {
 function backToHome() {
   navigate("../home/index.html");
 }
+
+validateForm();
